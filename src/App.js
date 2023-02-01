75f85e87cadb2855846d45bb55d2a47f3f0f5cb5
Ebele <ebele.omanukwue@telus.com>
2023-02-01 13:35:09 -0700

    create new routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/header/header';
 import './App.css';
  import SimpleBottomNavigation from './components/MainNav.js';
 import { Container } from '@mui/system';
+import Trending from './components/Trending/Trending';
+import Movies from './components/Movies/Movies';
+import Series from './components/Series/Series';
+import Search from './components/Search/Search';
 
 function App() {
   return (
@@ -11,7 +15,12 @@ function App() {
       <Header/>
     <div className="App">
       <Container>
-       
+       <Routes>
+        <Route path='/' component={Trending} exact/>
+        <Route path='/movies' component={Movies} />
+        <Route path='/series' component={Series} />
+        <Route path='/search' component={Search} />
+       </Routes>
 
         
       </Container>
---
+++ src/components/Trending/Trending.js
@@ -0,0 +1,11 @@
+const Trending = () => {
+  return (
+    <div>
+      <span className='pageTitle'>Trending</span>
+
+    </div>
+  )
+
+}
+
+export default Trending
